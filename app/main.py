7a46b14f883b6179c2d8c35b4c0f88bbcde8c198
Ira <iramladanovych@gmail.com>
2024-10-30 09:50:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 class Animal:
-    hidden = False
     alive = []
 
     def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
+        self.hidden = False  # Set hidden as an instance variable
         Animal.alive.append(self)
 
     def __len__(self) -> int:
@@ -16,6 +16,23 @@ class Animal:
             f"Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
         )
+
+    def remove_if_dead(self) -> None:
+        if self.health <= 0:
+            Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @classmethod
+    def bite(cls, goal: Animal) -> None:
+        if isinstance(goal, Herbivore) and not goal.hidden:
+            goal.health -= 50
+            goal.remove_if_dead()
 
 
 class Herbivore(Animal):
